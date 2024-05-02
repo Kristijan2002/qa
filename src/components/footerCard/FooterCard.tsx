a4340ee5a0ab8b15eeee6b2df3b2256f1700784d
Kristijan Maksimovski <kikomaksimovski2002@gmail.com>
2024-05-03 01:19:34 +0200

    ..

--- src/components/footerCard/FooterCard.tsx
+++ src/components/footerCard/FooterCard.tsx
@@ -10,13 +10,22 @@ interface Props{
 }
 
 const FooterCard: React.FC<Props> = ({icon, title, subTitle, contact}) => {
+    const copyContactToClipboard = () => {
+        const tempInput = document.createElement('input');
+        tempInput.value = contact;
+        document.body.appendChild(tempInput);
+        tempInput.select();
+        document.execCommand('copy');
+        document.body.removeChild(tempInput);
+        alert('Contact copied to clipboard!');
+      };
 
     return (
      <div className='w-full bg-custom-bgGray rounded-xl p-4'>
       <span className='mb-8'>{icon}</span>
       <h1 className='text-2xl'>{title}</h1>
       <p className='text-slate-500 mb-4'>{subTitle}</p>
-      <p className='text-custom-green'>{contact}</p>
+      <p className='text-custom-green' onClick={copyContactToClipboard}>{contact}</p>
      </div>
     );
 };
