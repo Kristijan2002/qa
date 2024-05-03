eea3f4c2cdb6e928867dc7d0c81df630eb61df16
Kristijan Maksimovski <kikomaksimovski2002@gmail.com>
2024-05-03 10:38:52 +0200

    ..

--- src/components/ourSolution/OurSolution.tsx
+++ src/components/ourSolution/OurSolution.tsx
@@ -7,10 +7,12 @@ interface SolutionProps{
 const OurSolution: React.FC<SolutionProps> = ({text, title}) => {
 
     return (
-        <div className="bg-custom-apricot sm:bg-custom-apricot w-full sm:w-1/2 text-left mx-auto p-4 pb-36 rounded-b-3xl flex flex-col pt-10">
+        <div className="bg-custom-apricot sm:bg-custom-apricot w-11/12 sm:w-1/2 md:w-4/5 text-left mx-auto p-4 pb-36 rounded-b-3xl flex flex-col pt-10">
+            <div className="w-full md:w-4/5 mx-auto">
                 <h1 className="text-2xl md:text-3xl lg:text-4xl xl:text-5xl 2xl:text-6xl">{title}</h1>
                 <p className="font-medium">{text}</p>
                 <img src="/465645 1.png" alt="" className="w-full mt-4"/>
+            </div>
         </div>
     );
 };
